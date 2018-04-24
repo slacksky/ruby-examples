38153475ab9b761c0f713e76e2446b437a2f0849
sky <slacksky@gmail.com>
2018-04-24 12:52:09 -0400

    heritage and modules examples

---
+++ Simulador_d_patos.rb
@@ -0,0 +1,40 @@
+class Pato
+  def nadar
+    puts "nada moviendo la colita"
+  end
+  def hacer_quak
+    puts "Quak..Quak..QUAK"
+  end
+  def volar
+    puts "Alzando el vuelo"
+  end
+  def mostrar
+    puts "Es un pato, que esperabas?"
+  end
+end
+
+class Mallard < Pato
+  def mostrar
+    super
+    puts "Es un pato que parece cura o una pata marron con manchas"
+  end
+end
+  class Blanco < Pato
+    def mostrar
+      super
+      puts "Es aburrido pato Blanco, nada especial"
+    end
+end
+
+pato = Pato.new
+lucas = Mallard.new
+donald = Blanco.new
+
+lucas.mostrar
+donald.mostrar
+pato.mostrar
+lucas.hacer_quak
+donald.hacer_quak
+pato.hacer_quak
+pato.volar
+lucas.nadar
--- modulo.rb
+++ modulo.rb
@@ -1,5 +1,25 @@
-def saludo nombre
-  puts "Hola! #{nombre}!"
+module Area
+  PI = 3.14
+  class Circulo
+    def self.definicion
+      "#{PI} por radio al cuadrado"
+    end
+  end
+  def Area.cuadrado
+    "lado del ciculo al cuadrado"
+  end
+
 end
 
-saludo "Juana"
+module Perimetro
+  PI = 3.14
+  class Circulo
+    def self.definicion
+      "2 veces #{PI} por radio"
+    end
+  end
+end
+
+
+puts Area::Circulo.definicion
+puts Perimetro::Circulo.definicion

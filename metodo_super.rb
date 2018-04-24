dffc485e0a6d1eb91263cc3ac07a716fabe02c6c
sky <slacksky@gmail.com>
2018-04-23 20:45:49 -0400

    updateing super method example

--- metodo_super.rb
+++ metodo_super.rb
@@ -4,17 +4,25 @@ class Persona
   def initialize(nombre)
     @nombre=nombre
   end
+  def dormir(n)
+    puts "Duerme #{n} horas"
+  end
 end
 
 class Empleado < Persona
   def initialize
     super('Juan')
   end
+  def dormir
+    super(8)
+  end
 end
 
 persona = Persona.new('jorge')
 
-empleado = Empleado.new('dan')
+empleado = Empleado.new
 
 puts persona.nombre
 puts empleado.nombre
+puts persona.dormir(12)
+puts empleado.dormir

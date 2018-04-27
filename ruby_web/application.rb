8656df5c38e7bedaa13b3acdc81900b90514757a
sky <slacksky@gmail.com>
2018-04-27 17:39:08 -0400

    updated example with ERB

--- ruby_web/application.rb
+++ ruby_web/application.rb
@@ -3,8 +3,9 @@ require "erb"
 class Application
   def self.call(env)
     if env['PATH_INFO'] == '/'
+      @mensaje = 'Hola mundo!'
       body = ERB.new(File.read('index.html.erb'))
-      [200, {}, [body.result]]
+      [200, {}, [body.result(binding)]]
     else
       [404, {}, 'No existe esta pagina']
     end
